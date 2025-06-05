149d63de35b361321c6600b35cdc05ced19e2cf2
basem.maher <basem.maher@gmail.com>
2025-06-05 12:41:06 +0300

    Squashed 'kafka-consumer/' content from commit af0ad48
    git-subtree-dir: kafka-consumer
    git-subtree-split: af0ad48a620355175b2cf025977c219f5bc617ae

---
+++ src/main/java/com/basem/consumer/consumer/config/KafkaConsumerConfig.java
@@ -0,0 +1,52 @@
+package com.basem.consumer.consumer.config;
+
+import com.basem.consumer.consumer.dto.Customer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, Customer> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "jt-group4");
+
+        // Use ErrorHandlingDeserializer wrapper
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+
+        // Inner deserializers
+        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+
+        // JSON settings
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.basem.consumer.consumer.dto");
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.basem.consumer.consumer.dto.Customer");
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Customer> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Customer> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
